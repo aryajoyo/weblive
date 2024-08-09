4caab788161522885c94fca5c9f7d96140a0849d
Arya Joyo <57989547+aryajoyo@users.noreply.github.com>
2024-08-09 13:39:10 +0700

    asdsd

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"AgoraRtcWrapper.bundle.js": "0b7c27838b79555efd162e131cf6fe5
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "324d38a9e8dfafedc504f4ce08613cce",
+"flutter_bootstrap.js": "b7f2cd936e1759f5c07646a720f5feae",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
